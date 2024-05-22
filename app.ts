6125ac76ebf5616dacad803db64e46b50cd6e02f
Danyella <danyellastrikann@duck.com>
2024-05-22 16:02:43 +0200

    feat: :sparkles: added svelte and sveltekit using sveltekit's node adapter
    added svelte and sveltekit
    changed scripts
    added dependencies from `bun create svelte@latest`
    formatted everything with prettier

--- app.ts
+++ app.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { coloring, superConsole } from './utils';
+import { handler } from './build/handler';
+import { superConsole } from './utils';
 
 // Verification checks
 if (!process.env.PORT) {
-    superConsole.warn('PORT is not defined in .env, switching to default port 3000.');
+	superConsole.warn('PORT is not defined in .env, switching to default port 3000.');
 }
 
 if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI is not defined in .env.');
+	throw new Error('MONGO_URI is not defined in .env.');
 }
 
 if (!process.env.DB_USER) {
-    throw new Error('DB_USER is not defined in .env.');
+	throw new Error('DB_USER is not defined in .env.');
 }
 
 if (!process.env.DB_PASSWD) {
-    throw new Error('DB_PASSWD is not defined in .env.');
+	throw new Error('DB_PASSWD is not defined in .env.');
 }
 
 const app = express();
@@ -24,24 +25,27 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (_req, res, next) => {
-    res.send('Hello, Adadas!');
-    next();
-});
+app.use(handler);
 
 app.use((req, res, next) => {
-    console.log('');
-    superConsole.info(`${req.method} - ${req.url} - ${res.statusCode}`, 'express.js');
-    next();
+	console.log('');
+	superConsole.info(`${req.method} - ${req.url} - ${res.statusCode}`, 'express.js');
+	next();
 });
 
-
 app.listen(PORT, () => {
-    superConsole.ready(`Server running on port ${PORT}.`, 'express.js');
-    mongoose.connect(process.env.MONGO_URI as string)
-        .then(() => {
-            superConsole.ready('Connected to MongoDB!', 'mongoose');
-            console.log('');
-            superConsole.ready('App started!');
-        }).catch(err => superConsole.error('An error occurred while trying to connect to MongoDB:\n    ' + err, 'mongoose'));
+	superConsole.ready(`Server running on port ${PORT}.`, 'express.js');
+	mongoose
+		.connect(process.env.MONGO_URI as string)
+		.then(() => {
+			superConsole.ready('Connected to MongoDB!', 'mongoose');
+			console.log('');
+			superConsole.ready('App started!');
+		})
+		.catch((err) =>
+			superConsole.error(
+				'An error occurred while trying to connect to MongoDB:\n    ' + err,
+				'mongoose'
+			)
+		);
 });
